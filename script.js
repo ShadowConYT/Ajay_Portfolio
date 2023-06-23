86c724c1905f468ce60117fc668945580c105faa
ShadowConYT <iamajay6122@gmail.com>
2023-06-23 22:50:05 +0530

    That's it I am Packing up

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 var texts = [
-    "Hello, World!",
-    "Welcome to my website.",
-    "This is a typewriter effect.",
-    "Feel free to modify and customize.",
+    "Web Developer",
+    "Data Analyst",
+    "Student",
+    "Often Watch Anime",
     "Enjoy!"
   ];
   
 // Get the typewriter container element
-var typewriterContainer = document.getElementById("typewriter-container");
+var typewriterContainer = document.getElementById("subtext");
 
-// Function to display random texts in typewriter effect
 function displayRandomText() {
-// Get a random index from the texts array
-var randomIndex = Math.floor(Math.random() * texts.length);
-
-// Get the random text
-var randomText = texts[randomIndex];
-
-// Set the random text as the content of the typewriter container
-typewriterContainer.textContent = randomText;
-}
-
-// Call the function initially
+    // Get a random index from the texts array
+    var randomIndex = Math.floor(Math.random() * texts.length);
+    
+    // Get the random text
+    var randomText = texts[randomIndex];
+    
+    // Clear the previous text
+    typewriterContainer.textContent = "";
+      
+      // Loop through each letter of the random text
+      for (var i = 0; i < randomText.length; i++) {
+        // Create a span element for each letter
+        var letterSpan = document.createElement("span");
+        
+        // Set the text content of the span to the current letter
+        letterSpan.textContent = randomText[i];
+        
+        // Append the span to the typewriter container
+        typewriterContainer.appendChild(letterSpan);
+      }
+    }
+    
+    // Call the function initially
 displayRandomText();
 
 // Call the function every 5 seconds to display a new random text
